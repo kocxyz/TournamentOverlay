c5c97e91f1ee29baa5e9554c5150cad51959b83b
Ipmake <niko.denis727@gmail.com>
2023-07-09 17:46:42 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,88 @@
-import logo from './logo.svg';
+import { Box, Typography } from '@mui/material';
 import './App.css';
+
+// Components
+import LinkCard from './components/LinkCard';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Box style={{
+      display: 'flex',
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      alignItems: 'space-between',
+      paddingTop: '1rem',
+    }}>
+      <Box style={{
+        textAlign: 'left',
+      }}>
+        {/* //////////////////////////////////// */}
+        {/* LEFT */}
+        {/* //////////////////////////////////// */}
+        <Box className="RoundHeaderLeft">
+          <Typography
+              fontFamily="Brda"
+              fontSize="48px"
+              fontWeight="bold"
+              letterSpacing="0.1em"
+          >GAME <strong>1</strong></Typography>
+        </Box>
+        <Box className="SubRoundHeaderLeft">
+          <Typography
+              fontFamily="Brda"
+              fontSize="38px"
+              fontWeight="bold"
+              letterSpacing="0.08em"
+          >ROUND <strong>1</strong></Typography>
+        </Box>
+
+      </Box>
+
+      <Box style={{
+        textAlign: 'right',
+      }}>
+        {/* //////////////////////////////////// */}
+        {/* RIGHT */}
+        {/* //////////////////////////////////// */}
+
+        <Box className="RoundHeaderRightBlue RightTextStraight">
+          <Typography
+              fontFamily="Brda"
+              fontSize="38px"
+              fontWeight="bold"
+              letterSpacing="0.00em"
+          >Pasiloy</Typography>
+          <Typography
+              fontFamily="Brda"
+              fontSize="38px"
+              fontWeight="bold"
+              letterSpacing="0.08em"
+          >0</Typography>
+        </Box>
+        <Box className="RoundHeaderRightYellow RightTextStraight">
+          <Typography
+              fontFamily="Brda"
+              fontSize="38px"
+              fontWeight="bold"
+              letterSpacing="0.02em"
+              textAlign="center"
+          >Chl0ekeil3r</Typography>
+          <Typography
+              fontFamily="Brda"
+              fontSize="38px"
+              fontWeight="bold"
+              letterSpacing="0.08em"
+          >0</Typography>
+        </Box>
+      </Box>
+
+    </Box>
+
+      {/* Bottom right tag */}
+
+      <LinkCard />
+    </>
   );
 }
 
---
+++ src/components/LinkCard.jsx
@@ -0,0 +1,32 @@
+import { Box, Typography } from '@mui/material';
+import { LinkOutlined } from '@mui/icons-material';
+import { useEffect, useState } from 'react';
+
+function LinkCard() {
+    const [adOpen, setAdOpen] = useState(false);
+
+    useEffect(() => {
+      const interval = setInterval(() => {
+        setAdOpen(true);
+        setTimeout(() => {
+          setAdOpen(false);
+        }, 7500)
+      }, 15000);
+  
+      return () => clearInterval(interval);
+    }, []);
+
+    return (
+        <Box className="BottomRightTag shimmer" style={{
+            bottom: '0',
+            right: adOpen ? '0' : '-300px',
+        }}>
+        <LinkOutlined style={{
+            transform: 'rotate(-45deg)',
+        }} />
+        <Typography fontFamily="Brda" fontSize="28px" fontWeight="bold" letterSpacing="0.08em">kocity.xyz</Typography>
+        </Box>
+    )
+}
+
+export default LinkCard
